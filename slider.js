49cb20716525eeedd11221ac11dea3c190a0b10d
Puukallistaja <haponen@gmail.com>
2017-10-23 17:38:09 +0200

    spacer moves when scroll event fired

--- slider.js
+++ slider.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $('#slider').slider({
+  const sl = $('#slider').slider({
     orientation: "vertical",
     // range: "max",
     min: 10,
@@ -11,11 +11,42 @@ $(document).ready(function(){
       console.log(pos)
       $('#spacer').css({'top': `${pos}%`})
 
+      //move the cover divs with the slider
       if (ui.value > 50) {
         $('.top').height(`${pos}%`)
       } else {
         $('.bottom').height(`${100 - pos}%`)
       }
+
+      
     }
+  });
+
+  $('body').on( 'DOMMouseScroll mousewheel', function ( event ) {
+    let currHeight = sl.slider('option', 'value');
+    let newHeight;
+
+    function slideWithScroll(height) {
+      console.log(currHeight, height)
+      sl.slider('option', 'value', height);
+      sl.slider('option','slide')
+          .call(sl, null, { handle: $('.ui-slider-handle', sl), value: height });
+    }
+
+    if ( event.originalEvent.detail     > 0 
+      || event.originalEvent.wheelDelta < 0 ) {
+      //scroll down
+      newHeight = currHeight >= 12 ? currHeight -2
+                                  : currHeight
+      slideWithScroll(newHeight);
+
+    } else {
+      //scroll up
+      newHeight = currHeight <= 88 ? currHeight + 2
+                                   : currHeight
+      slideWithScroll(newHeight);
+    }
+    //prevent page fom scrolling
+    return false;
   });
 });
